6119922e8da97f29971f6c3edb971e6758d8aa97
stanimir-georgiev <stanimir.georgiev_99@abv.bg>
2021-04-05 19:25:25 +0300

    Register the new SW

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }) {
   useEffect(() => {
     const cachebuster = Math.round(new Date().getTime() / 1000);
     console.log("Registered service worker", cachebuster);
-    register(`/service-worker.js`, { scope: "/" });
+    register(`/service-worker-2.js`, { scope: "/" });
     return () => {
       unregister();
     };
